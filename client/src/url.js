a4620fd836a605f1ba66faaaed32610f385a41fb
_OJ_ <53382501+OJrogue@users.noreply.github.com>
2022-04-30 22:53:32 +0200

    Update url.js

--- client/src/url.js
+++ client/src/url.js
@@ -1,4 +1,3 @@
 // export const url = 'http://localhost:5000';
-export const url = 'https://discordtasktracker.herokuapp.com/';
-export const discord_url =
-  'https://discord.com/api/oauth2/authorize?client_id=969922012199931904&redirect_uri=https%3A%2F%2Fdiscord-task-tracker.netlify.app%2Flogin&response_type=code&scope=identify';
+export const url = ''; // api endpoint
+export const discord_url = ''; // discord auth url
